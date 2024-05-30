180d3c347cd3cde848854c1dbd3fcd8abb143bb2
Binliau <kevinraihan.work@gmail.com>
2024-05-30 16:41:03 +0700

    Added Product Route

---
+++ Backend/routes/ProductRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const productController = require("../repositories/repository.product");
+const router = express.Router();
+
+//STARTS WITH /product
+router.get("/products", productController.getProducts);
+router.get("/product/:id", productController.getProduct);
+router.post("/product", productController.addProduct);
+router.put("/product/:id", productController.updateProduct)
+router.delete("/product/:id", productController.deleteProduct)
+
+module.exports = router;
