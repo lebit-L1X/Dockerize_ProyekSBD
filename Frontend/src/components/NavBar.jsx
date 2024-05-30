63a5c293aacfef2ac2465724df7331a346abf45f
rafliho30 <rafli.adithia@ui.ac.id>
2024-05-30 22:15:19 +0700

    Struktur Frontend

---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+
+const NavBar = () => {
+    return (
+        <nav className="flex items-center p-4 shadow-md">
+            <header className='text-xl items-center'>
+                <h1 className='text-red-500'>Fresh<span className="text-green-500">Market</span></h1>
+            </header>
+        </nav>
+    );
+};
+export default NavBar;
