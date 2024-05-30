aa8d2886ecf36ebdca97f898d5a4f8f590ba55f5
lebit-L1X <109059063+lebit-L1X@users.noreply.github.com>
2024-05-30 16:23:37 +0700

    add template files / structure. & models

---
+++ Backend/models/ProductModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
+    price: { type: Number, required: true, unique: false },
+    stock: { type: Number, required: true, unique: false },
+    supplier: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier",
+      },
+    ],
+    Paid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = {
+  Product,
+};
---
+++ Backend/models/SupplierModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const supplierSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  description: {type: String, required: false},
+  owed: { type: Number, default: 0},
+  productlist: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Product"
+  }]
+
+});
+
+const Supplier = mongoose.model("Supplier", supplierSchema);
+
+module.exports = {
+  Supplier,
+};
