d3de49f357b5df13db085ada6055f58a995beb9a
FathinAero <aerofathin000@gmail.com>
2024-05-31 00:22:11 +0700

    SupplierDetail

--- Frontend/src/components/StoreInfo.jsx
+++ Frontend/src/components/StoreInfo.jsx
@@ -1,10 +1,10 @@
 const ShopInfo = () => {
-    return (
-        <div className="">
-            <h1>Store Info</h1>
-            <p>Saldo : </p>
-            <p>Shop cooking</p>
-        </div>
-    );
-}
+  return (
+    <div className="">
+      <h1>Store Info</h1>
+      <p>Saldo : </p>
+      <p>Shop cooking</p>
+    </div>
+  );
+};
 export default ShopInfo;
